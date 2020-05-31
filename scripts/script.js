ddfb285cdde9e22cffc8771c1f807854a95dce4a
Surajan Shrestha <surajanshrestha35@gmail.com>
2020-05-31 10:30:24 +0545

    JS DOM Added

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,9 @@ const url='https://surajan-heroku-test.herokuapp.com/';
 xhr.responseType='json';
 xhr.onreadystatechange=()=>{
   if(xhr.readyState===XMLHttpRequest.DONE){
-    console.log(typeof xhr.response);
     console.log(xhr.response);
+    document.getElementsByTagName("textarea")[0].innerHTML='Name: '+xhr.response.name+'\n'+'Age: '+xhr.response.age+'\n'+'Gender: '
+    +xhr.response.gender+'\n'+'Height: '+xhr.response.height;
   }
 };
 xhr.open('GET',url);
